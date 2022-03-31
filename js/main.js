5576ae09314e02549b61f401ab905b7b800301ff
itzJunaedRayhan <webgenesis99@gmail.com>
2022-04-01 02:43:59 +0600

    Website Completed

--- js/main.js
+++ js/main.js
@@ -2,18 +2,43 @@ const navItems = document.querySelector('#nav_items');
 const openNavBtn = document.querySelector('#open_nav-btn');
 const closeNavBtn = document.querySelector('#close_nav-btn');
 
+//  Open Navbar on click:
 openNavBtn.addEventListener('click', () => {
     navItems.style.display = 'flex';
     openNavBtn.style.display = 'none';
     closeNavBtn.style.display = 'inline-block';
 });
 
-closeNavBtn.addEventListener('click', () => {
+
+//  closeNav Function:
+function closeNav () {
     navItems.style.display = 'none';
     openNavBtn.style.display = "inline-block";
     closeNavBtn.style.display = 'none';
-});
+}
 
+
+//  Close Navbar on click:
+closeNavBtn.addEventListener('click', closeNav);
+
+
+//  close nav menu when menu item is clicked:
+if (window.innerWidth <= 1024) {
+    document.querySelectorAll('#nav_items li a').forEach(navItem => {
+        navItem.addEventListener('click', () => {
+            closeNav();
+        })
+    })
+}
+
+
+//  change navbar styles on scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+})
+
+
+//  Testimonial Section - Swiper Implementation:
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
